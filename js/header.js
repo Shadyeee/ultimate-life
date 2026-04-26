d7b8f0e255f455b87faa121243ad28e905db10d7
Shady Megahed <shadymegahed4@gmail.com>
2026-04-26 18:26:43 +0300

    v2

---
+++ js/header.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("header").innerHTML = `
+<header>
+  <div class="container nav">
+    <div class="logo">Ultimate Life</div>
+
+
+
+    <a class="btn" href="/#features">تواصل معانا</a>
+  </div>
+</header>
+
+`;
+});
